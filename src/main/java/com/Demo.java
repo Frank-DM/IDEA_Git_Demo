bd2f0f36c573054436112523f575347ade9d78a4
Frank <liudaoming_better@163.com>
2019-05-26 20:26:25 +0800

    Second Commite

--- src/main/java/com/Demo.java
+++ src/main/java/com/Demo.java
@@ -4,5 +4,7 @@ public class Demo {
 
     public static void main(String[] args) {
         System.out.println("This is class Demo");
+
+        System.out.println("Add in second commit");
     }
 }
